411a38c05fb2a0504028b7d52c7e43333b0d56e6
unknown <agustinraggi@gmail.com>
2024-06-03 20:04:46 -0300

    se acomodo el header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,22 +1,42 @@
 import React from "react"
 import "./Header.css"
 import { Link } from "react-router-dom"
-// import { useAuth } from "../../context/authContext"
+
 
 const Header = () => {
     // const auth = useAuth();
     // const displayName = auth.user?.displayName;
 
     return (
-        <div className="header">
-            <div className="headerLeft">
-                <Link to="/" style={{textDecoration: "none"}}><span>Cine Aurora</span></Link>
-                <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Cartelera</span></Link>
-                <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Proximamente</span></Link>
-                <Link to="/crudUser" style={{textDecoration: "none"}}><span>Usuario</span></Link>
-                {/* <span id="userActive">{displayName}</span> */}
-            </div>
+            <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+        <Link to="/" style={{textDecoration: "none"}} className="navbar-brand"><span>Cine Aurora</span></Link>
+          <button
+            class="navbar-toggler ms-auto"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul id="barra__tareas" class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+              <Link to="/movies/popular" style={{textDecoration: "none"}} className="navbar-brand"><span>Cartelera</span></Link>
+              </li>
+              <li class="nav-item">
+              <Link to="/movies/upcoming" style={{textDecoration: "none"}} className="navbar-brand"><span>Proximamente</span></Link>
+              </li>
+              <li class="nav-item">
+              <Link to="/crudUser" style={{textDecoration: "none"}} className="navbar-brand"><span>Usuario</span></Link>
+              </li>
+            </ul>
+          </div>
         </div>
+      </nav>
     )
 }
 
