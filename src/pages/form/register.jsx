4b98e16af7487fd83b6c18aa8ee7c4e360dbcd08
unknown <agustinraggi@gmail.com>
2024-06-06 00:49:25 -0300

    se finalizo el crud del usuario

--- src/pages/form/register.jsx
+++ src/pages/form/register.jsx
@@ -4,42 +4,143 @@ import "react-datepicker/dist/react-datepicker.css";
 import "./register.css";
 import { Link } from "react-router-dom";
 import axios from 'axios';
+import Swal from 'sweetalert2'
+
 
 function User() {
     const [filteredPeople, setFilteredPeople] = useState([]);
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [search, setSearch] = useState("");
     const [editIndex, setEditIndex] = useState(null);
+    const [id, setId] = useState(null);
 
-    const [correo, setCorreo] = useState("");
-    const [nombre, setNombre] = useState("");
+    const [mail, setMail] = useState("");
+    const [name, setName] = useState("");
     const [dni, setDni] = useState("");
     const [password, setPassword] = useState("");
 
     const [listPeople, setListPeople] = useState([]);
 
+    // REGISTRAR USUARIO
     const add = () => {
-        if (validateForm()) {
-            const formattedDate = selectedDate.toISOString().split('T')[0];
+    if (validateForm()) {
+        const formattedDate = selectedDate.toISOString().split('T')[0];
+        axios.post("http://localhost:3001/create", {
+            mail,
+            name,
+            dni,
+            date: formattedDate,
+            password
+        })
+        .then(() => {
+            getCustomer();
+            clearForm();
+            Swal.fire({
+                title: "<strong>Usuario Registrado</strong>",
+                html: "<i>El usuario <strong>" + name + "</strong> fue REGISTRADO con éxito!</i>",
+                icon: "success",
+                timer: 2000
+            });
+        })
+        .catch((error) => {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "No se pudo registrar el usuario!",
+                footer: JSON.parse(JSON.stringify(error)).message === "Network Error" ? "Intente más tarde" : JSON.parse(JSON.stringify(error)).message
+            });
+        });
+    }
+}
 
-            axios.post("http://localhost:3001/create", {
-                correo: correo,
-                nombre: nombre,
-                dni: dni,
-                fecha: formattedDate,
-                password: password
-            }).then(() => {
-                alert("Usuario registrado!!!");
-                getClientes();
+
+// UPDATE
+const update = () => {
+    if (validateForm()) {
+        const formattedDate = selectedDate.toISOString().split('T')[0];
+        axios.put("http://localhost:3001/update", {
+            id,
+            mail,
+            name,
+            dni,
+            date: formattedDate,
+            password
+        })
+        .then(() => {
+            Swal.fire({
+                title: "<strong>Usuario Actualizado</strong>",
+                html: "<i>El usuario <strong>" + name + "</strong> fue ACTUALIZADO con éxito!</i>",
+                icon: "success",
+                timer: 2000
+            });
+            getCustomer();
+            clearForm();
+            setEditIndex(null);
+        })
+        .catch((error) => {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "No se pudo registrar el usuario!",
+                footer: JSON.parse(JSON.stringify(error)).message === "Network Error" ? "Intente más tarde" : JSON.parse(JSON.stringify(error)).message
+            });
+        });
+    }
+}
+
+
+// DELETE
+const deleteData = (id, name) => {
+    Swal.fire({
+        title: "¿Estás seguro?",
+        text: `No podrás revertir esta acción. ¿Estás seguro de que deseas eliminar a ${name}?`,
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Sí, eliminar!",
+        cancelButtonText: "No, cancelar",
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            axios.delete(`http://localhost:3001/delete/${id}`)
+            .then(() => {
+                Swal.fire({
+                    title: "¡Eliminado!",
+                    text: `El usuario ${name} ha sido eliminado.`,
+                    icon: "success",
+                    timer: 2000
+                });
+                getCustomer();
                 clearForm();
-            }).catch((error) => {
-                console.error("Hubo un error al registrar el usuario:", error);
+                setEditIndex(null);
+            })
+            .catch((error) => {
+                console.error("Hubo un error al eliminar el usuario:", error);
+            });
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+            Swal.fire({
+                title: "Cancelado",
+                text: "Tu archivo está a salvo :)",
+                icon: "error",
+                timer: 2000
             });
         }
-    }
+    })
+    .catch((error) => {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "No se pudo Eliminar Usuario!",
+            footer: JSON.parse(JSON.stringify(error)).message === "Network Error" ? "Intente mas tarde" : JSON.parse(JSON.stringify(error)).message
+        });
+    });
+}
 
-    const getClientes = () => {
-        axios.get("http://localhost:3001/cliente").then((response) => {
+
+
+
+    // MOSTRAR DATOS
+    const getCustomer = () => {
+        axios.get("http://localhost:3001/customer").then((response) => {
             setListPeople(response.data);
             setFilteredPeople(response.data);
         }).catch((error) => {
@@ -48,19 +149,20 @@ function User() {
     }
 
     useEffect(() => {
-        getClientes();
+        getCustomer();
     }, []);
 
     useEffect(() => {
         filterPeople();
     }, [search, listPeople]);
 
+    // VALIDACIONES DE FORMULARIO
     const validateForm = () => {
-        if (!correo || !correo.includes('@')) {
+        if (!mail || !mail.includes('@')) {
             alert('Por favor complete un correo electrónico válido');
             return false;
         }
-        if (!nombre) {
+        if (!name) {
             alert('Por favor complete el nombre');
             return false;
         }
@@ -80,33 +182,35 @@ function User() {
     }
 
     const clearForm = () => {
-        setCorreo("");
-        setNombre("");
+        setId(null);
+        setMail("");
+        setName("");
         setDni("");
         setPassword("");
         setSelectedDate(new Date());
+        setEditIndex(null);
     }
 
     const filterPeople = () => {
-        const filtered = listPeople.filter(person =>
-            person.nombre.toLowerCase().includes(search.toLowerCase())
-        );
-        setFilteredPeople(filtered);
+        if (listPeople) {
+            const filtered = listPeople.filter(client =>
+                client.name.toLowerCase().includes(search.toLowerCase())
+            );
+            setFilteredPeople(filtered);
+        }
     }
 
-    const editData = (index) => {
-        const person = filteredPeople[index];
-        setCorreo(person.correo);
-        setNombre(person.nombre);
-        setDni(person.dni);
-        setSelectedDate(new Date(person.fecha));
-        setPassword(person.password);
-        setEditIndex(index);
+    // EDIT
+    const editData = (val) => {
+        setEditIndex(true);
+        setId(val.id);
+        setMail(val.mail);
+        setName(val.name);
+        setDni(val.dni);
+        setSelectedDate(new Date(val.date));
+        setPassword(val.password);
     }
 
-    const deleteData = (index) => {
-        const person = filteredPeople[index];
-    }
 
     return (
         <div className="container">
@@ -116,11 +220,11 @@ function User() {
                         <h1>REGISTRAR USUARIO</h1>
                         <div className="login-form">
                             <label className="form-label" id="text">Correo Electrónico</label>
-                            <input onChange={(event) => setCorreo(event.target.value)} value={correo} type="email" className="form-control" id="inputEmail" placeholder="Ingrese su correo electrónico" />
+                            <input onChange={(event) => setMail(event.target.value)} value={mail} type="email" className="form-control" id="inputEmail" placeholder="Ingrese su correo electrónico" />
                         </div>
                         <div className="login-form">
                             <label className="form-label" id="text">Nombre Completo</label>
-                            <input onChange={(event) => setNombre(event.target.value)} value={nombre} type="text" className="form-control" id="inputName" placeholder="Ingrese su nombre completo" />
+                            <input onChange={(event) => setName(event.target.value)} value={name} type="text" className="form-control" id="inputName" placeholder="Ingrese su nombre completo" />
                         </div>
                         <div className="login-form">
                             <label className="form-label" id="text">DNI</label>
@@ -149,18 +253,26 @@ function User() {
                             <label className="form-label" id="text">Contraseña</label>
                             <input onChange={(event) => setPassword(event.target.value)} value={password} type="password" className="form-control" id="inputPassword" placeholder="*****" />
                         </div>
-                        <button type="button" className="Btn btn btn-primary" id="btnAdd" onClick={add}>Enviar</button>
+                        {
+                            editIndex !== null ?
+                                <div>
+                                    <button type="button" className="Btn btn-primary" id="btnUpdate" onClick={update}>Actualizar</button>
+                                    <button type="button" className="Btn btn-primary" id="btnCancel" onClick={clearForm}>Cancelar</button>
+                                </div>
+                                :
+                                <button type="button" className="Btn btn btn-primary" id="btnAdd" onClick={add}>Enviar</button>
+                        }
                         <p>Ingresar aquí <Link to="/login">aquí</Link></p>
                     </form>
                 </div>
             </div>
             <div className="btnSearch">
-                <input 
-                    type="text" 
-                    className="form-control-search" 
-                    placeholder="Buscar por nombre" 
-                    value={search} 
-                    onChange={e => setSearch(e.target.value)} 
+                <input
+                    type="text"
+                    className="form-control-search"
+                    placeholder="Buscar por nombre"
+                    value={search}
+                    onChange={e => setSearch(e.target.value)}
                 />
             </div>
             <hr />
@@ -177,17 +289,20 @@ function User() {
                     </tr>
                 </thead>
                 <tbody>
-                    {filteredPeople.map((person, index) => (
-                        <tr key={person.id}>
-                            <td>{person.correo}</td>
-                            <td>{person.nombre}</td>
-                            <td>{person.dni}</td>
-                            <td>{new Date(person.fecha).toLocaleDateString()}</td>
-                            <td>{person.edad}</td>
-                            <td>{person.password}</td>
+                    {filteredPeople.map((client, index) => (
+                        <tr key={client.id}>
+                            <td>{client.mail}</td>
+                            <td>{client.name}</td>
+                            <td>{client.dni}</td>
+                            <td>{new Date(client.date).toLocaleDateString()}</td>
+                            <td>{client.age}</td>
+                            <td>{client.password}</td>
                             <td>
-                                <button className="btn btn-warning" onClick={() => editData(index)}>Editar</button>
-                                <button className="btn btn-danger" onClick={() => deleteData(index)}>Eliminar</button>
+                                <button className="btn btn-warning"
+                                    onClick={() => {
+                                        editData(client);
+                                    }}>Editar</button>
+                                <button className="btn btn-danger" onClick={() => deleteData(client.id, client.name)}>Eliminar</button>
                             </td>
                         </tr>
                     ))}
