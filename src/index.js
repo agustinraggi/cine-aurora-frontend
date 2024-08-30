d514ff58e9ba5cf24f48c84e9cc25d2afd55e041
Agustin Raggi <agustinraggi@gmail.com>
2024-08-30 11:44:58 -0300

    agregamos las librerias DatePicker y acomodamos las viejas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {MuiPickersUtilsProvider} from '@material-ui/pickers'
+import DateFnsUtils from '@date-io/date-fns'
 
 ReactDOM.render(
-    <App />,
+    <MuiPickersUtilsProvider utils ={DateFnsUtils}>
+      <App />
+    </MuiPickersUtilsProvider>,
   document.getElementById('root')
 )
